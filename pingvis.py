e0ddd3bbd1ba167c0b35ef285b0e05cfc928ea28
Matt <vultour@users.noreply.github.com>
2015-10-02 17:53:47 +0100

    Added variable window size support and command line switches for most settings

--- pingvis.py
+++ pingvis.py
@@ -5,21 +5,22 @@ import time
 import sys
 import re
 
-HELP_MSG	= sys.argv[0]+" --address <IP> [--interval <X.Y>] [--max-ping <X>] [--history <X>]"
+HELP_MSG	= sys.argv[0]+" --address <IP> [--interval <X.Y>] [--max-ping <X>] [--history <X>] [--window-height <X>] [--window-width <X>] [--fullscreen] [--ping-threshold <X>]"
 
-IP_ADDR		= "0.0.0.0"
-CLOCK_DELTA	= 0.1
-MAX_PING	= 500
-HISTORY_SIZE	= 600.0
-COORD_MULT	= {"X": 1, "Y": 1}
-RESOLUTION	= (0, 0)
+IP_ADDR		= "0.0.0.0" # Default IP
+CLOCK_DELTA	= 0.1 # Minimal refresh rate
+MAX_PING	= 500.0 # The visualization will automatically scale to display 0 ping on top and MAX_PING ping at the bottom
+HISTORY_SIZE	= 600.0 # Number of history values to display
+COORD_MULT	= {"X": 1, "Y": 1} # Coord scaler, automatically determined based on window size, MAX_PING and HISTORY_SIZE
+RESOLUTION	= (640, 480) # Default window size
 HISTORY		= []
 
-TEXT_COLOUR	= (180, 180, 180)
+TEXT_COLOR	= (180, 180, 180) # Infotext color
 
-LAG_THRESHOLD	= [
-			{"ping": 60,  "colour": (0, 160, 0), "desc": "Counter-Strike lag"},
-			{"ping": 250, "colour": (90, 90, 200), "desc": "Streaming lag"}
+PING_THRESHOLD	= 250 # Ping above this value will raise the INTERNET DYING warning
+LAG_THRESHOLD	= [ # You can define custom ping thresholds that will display lines across the screen
+			{"ping": 60,  "color": (0, 160, 0), "desc": "Counter-Strike lag"},
+			{"ping": 250, "color": (90, 90, 200), "desc": "Streaming lag"}
 		]
 
 def getNextArg():
@@ -33,6 +34,8 @@ def parseArgs():
 	global MAX_PING
 	global CLOCK_DELTA
 	global HISTORY_SIZE
+	global RESOLUTION
+	global PING_THRESHOLD
 
 	try:
 		while (True):
@@ -43,16 +46,24 @@ def parseArgs():
 			elif (a == "--address"):
 				IP_ADDR = getNextArg()
 			elif (a == "--max-ping"):
-				MAX_PING = getNextArg()
+				MAX_PING = float(getNextArg())
 			elif (a == "--interval"):
-				CLOCK_DELTA = getNextArg()
+				CLOCK_DELTA = float(getNextArg())
 			elif (a == "--history"):
-				HISTORY_SIZE = getNextArg()
+				HISTORY_SIZE = float(getNextArg())
+			elif (a == "--fullscreen"):
+				RESOLUTION = (0, 0)
+			elif (a == "--window-width"):
+				RESOLUTION = (int(getNextArg()), RESOLUTION[1])
+			elif (a == "--window-height"):
+				RESOLUTION = (RESOLUTION[0], int(getNextArg()))
+			elif (a == "--ping-threshold"):
+				PING_THRESHOLD = float(getNextArg())
 	except IndexError:
 		pass
 
 def normalizePoints(p):
-	i = 1
+	i = 0
 	ret = []
 	for point in p:
 		ret.append((i*COORD_MULT["X"], point*COORD_MULT["Y"]))
@@ -89,7 +100,11 @@ def main():
 	CLOCK = -10.0
 
 	pygame.init()
-	screen = pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
+	pygame.display.set_caption("PingVis - "+IP_ADDR)
+	if (RESOLUTION == (0, 0)):
+		screen = pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
+	else:
+		screen = pygame.display.set_mode(RESOLUTION)
 	font = pygame.font.SysFont("monospace", 15)
 
 	screen.blit(font.render("LOADING", True, (255, 255, 255)), (10, 10))
@@ -118,18 +133,18 @@ def main():
 			if (len(HISTORY) > 1):
 				screen.fill((0, 0, 0))
 				for l in LAG_THRESHOLD:
-					pygame.draw.lines(screen, l["colour"], False, [(0, l["ping"]*COORD_MULT["Y"]), (screen.get_width(), l["ping"]*COORD_MULT["Y"])], 1)
-					screen.blit(font.render(l["desc"], 1, l["colour"]), (5, l["ping"]*COORD_MULT["Y"]))
+					pygame.draw.lines(screen, l["color"], False, [(0, l["ping"]*COORD_MULT["Y"]), (screen.get_width(), l["ping"]*COORD_MULT["Y"])], 1)
+					screen.blit(font.render(l["desc"], 1, l["color"]), (5, l["ping"]*COORD_MULT["Y"]))
 
-				screen.blit(font.render("Ping: "+str(HISTORY[len(HISTORY)-1])+"ms", 1, TEXT_COLOUR), (5, screen.get_height()-20))
+				screen.blit(font.render("Ping: "+str(HISTORY[len(HISTORY)-1])+"ms", 1, TEXT_COLOR), (5, screen.get_height()-20))
 				if (HISTORY[len(HISTORY)-1] < 0):
 					screen.blit(font.render("UNREACHABLE", 1, (255, 0, 0)), (120, screen.get_height()-20))
-				screen.blit(font.render("MULT_X: "+str(COORD_MULT["X"])+", MULT_Y: "+str(COORD_MULT["Y"]), 1, TEXT_COLOUR), (5, screen.get_height()-40))
-				screen.blit(font.render("IP Address: "+IP_ADDR, 1, TEXT_COLOUR), (5, screen.get_height()-60))
-				screen.blit(font.render("Min interval: "+str(CLOCK_DELTA), 1, TEXT_COLOUR), (5, screen.get_height()-80))
-				screen.blit(font.render("Clock: "+str(CLOCK), 1, TEXT_COLOUR), (5, screen.get_height()-100))
-				screen.blit(font.render("Max ping: "+str(MAX_PING), 1, TEXT_COLOUR), (5, screen.get_height()-120))
-				screen.blit(font.render("History size: "+str(HISTORY_SIZE), 1, TEXT_COLOUR), (5, screen.get_height()-140))
+				screen.blit(font.render("MULT_X: "+str(COORD_MULT["X"])+", MULT_Y: "+str(COORD_MULT["Y"]), 1, TEXT_COLOR), (5, screen.get_height()-40))
+				screen.blit(font.render("IP Address: "+IP_ADDR, 1, TEXT_COLOR), (5, screen.get_height()-60))
+				screen.blit(font.render("Min interval: "+str(CLOCK_DELTA), 1, TEXT_COLOR), (5, screen.get_height()-80))
+				screen.blit(font.render("Clock: "+str(CLOCK), 1, TEXT_COLOR), (5, screen.get_height()-100))
+				screen.blit(font.render("Max ping: "+str(MAX_PING), 1, TEXT_COLOR), (5, screen.get_height()-120))
+				screen.blit(font.render("History size: "+str(HISTORY_SIZE), 1, TEXT_COLOR), (5, screen.get_height()-140))
 				
 				pygame.draw.lines(screen, (255, 0, 0), False, normalizePoints(HISTORY), 1)
 				pygame.display.update()
